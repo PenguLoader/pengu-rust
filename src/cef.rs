7ad8e27ce30be4a6193d6589328ffe93a275639b
Nguyen Duy <wuuyi123@gmail.com>
2024-08-31 20:21:03 +0700

    add winapi core

--- src/cef.rs
+++ src/cef.rs
@@ -8,3 +8,24 @@
 #![allow(unused_variables)]
 
 include!(concat!(env!("OUT_DIR"), "/cef_bindings.rs"));
+
+use std::{
+    mem::MaybeUninit,
+    os::{raw::c_void, windows::raw::HANDLE},
+    ptr::null_mut,
+    sync::atomic::{AtomicUsize, Ordering},
+    usize,
+};
+
+use winapi::{
+    shared::ntdef::LPCWSTR,
+    um::{
+        errhandlingapi::GetLastError,
+        handleapi::CloseHandle,
+        processthreadsapi::{CreateProcessW, ResumeThread, PROCESS_INFORMATION, STARTUPINFOW},
+        synchapi::WaitForSingleObject,
+        winbase::{CREATE_SUSPENDED, DEBUG_ONLY_THIS_PROCESS, INFINITE},
+        winnt::LPCSTR,
+        winuser::{MessageBoxA, MessageBoxW},
+    },
+};
