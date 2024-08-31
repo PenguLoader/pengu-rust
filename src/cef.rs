6ed1984dab97ea7f65724045046a783ffe2f5b74
Nguyen Duy <wuuyi123@gmail.com>
2024-08-31 20:15:01 +0700

    init cef bindgen

---
+++ src/cef.rs
@@ -0,0 +1,10 @@
+#![allow(clippy::all)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(deref_nullptr)]
+#![allow(unused)]
+#![allow(unused_unsafe)]
+#![allow(unused_variables)]
+
+include!(concat!(env!("OUT_DIR"), "/cef_bindings.rs"));
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
+mod cef;
 
 fn main() {
-    
 }
